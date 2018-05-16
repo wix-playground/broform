199a8c6ec3f32eacc7450125a5f4536fcd99eb89
iaroslav <iaroslavs@wix.com>
2018-05-16 23:04:29 +0300

    simple test

--- __tests__/test-example.tsx
+++ __tests__/test-example.tsx
@@ -2,6 +2,6 @@ import * as React from 'react';
 import {shallow} from 'enzyme';
 
 test('render a label', () => {
-  const wrapper = shallow(<label>Hello Jest!</label>);
+  const wrapper = shallow(<label>Hello Jest</label>);
   expect(wrapper).toMatchSnapshot();
 });
--- test/jest-setup.ts
+++ test/jest-setup.ts
@@ -1,22 +1,7 @@
-import * as jsdomRegistrar from 'jsdom-global';
 import {configure} from 'enzyme';
 import * as Adapter from 'enzyme-adapter-react-15';
 
 const createEnvironment = () => {
-  const documentHTML = `
-    <!doctype html>
-    <html>
-      <head></head>
-      <body>
-        <div id="test-react-root"></div>
-      </body>
-    </html>
-  `;
-
-  jsdomRegistrar(documentHTML, {
-    pretendToBeVisual: true,
-  });
-
   configure({
     adapter: new Adapter(),
   });
